e68a9bb058668a46a5cf16566997647cc62b2cdf
shivamgpt01 <shivamgpt01@gmail.com>
2024-08-05 17:55:02 +0530

    Added movie list in secondary containers, css changes, separate hooks for each movie category

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,16 +3,22 @@ import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies.js";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies.js";
+import useTopRatedMovies from "../hooks/useTopRatedMovies.js";
+import useUpcomingMovies from "../hooks/useUpcomingMovies.js";
 
 const Browse = () => {
   //Custom Hook to Fetch Data from TMDB API and update Store
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
 
   return (
     <div>
       <Header />
-      <MainContainer/>
-      <SecondaryContainer/>
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   );
 };
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
   return (
-    <div>SecondaryContainer</div>
-  )
-}
+    movies.nowPlayingMovies && (
+      <div className="bg-black">
+        <div className="relative z-20 -mt-48 pl-12">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+          <MovieList title={"Popular"} movies={movies.popularMovies} />
+          <MovieList title={"Upcoming"} movies={movies.upcomingMovies} />
+        </div>
+      </div>
+    )
+  );
+};
 
-export default SecondaryContainer
+export default SecondaryContainer;
